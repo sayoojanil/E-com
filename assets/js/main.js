7ab2cc69e99e603923a431965cec756a261ca098
sayooj anil <sayoojbgmi@gmail.com>
2025-10-29 20:16:57 +0530

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,9 +19,9 @@ function renderRatingStars(rating) {
   const full = Math.floor(rating);
   const half = rating - full >= 0.5;
   let html = '';
-  for (let i = 0; i < full; i++) html += '<i class="fa-solid fa-star"></i>';
-  if (half) html += '<i class="fa-solid fa-star-half-stroke"></i>';
-  for (let i = full + (half ? 1 : 0); i < 5; i++) html += '<i class="fa-regular fa-star"></i>';
+  for (let i = 0; i < full; i++) html += '';
+  if (half) html += '';
+  for (let i = full + (half ? 1 : 0); i < 5; i++) html += '';
   return html;
 }
 
